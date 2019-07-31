6363e2b87a3e0a57ba7e03ecbee6816d1b4adef9
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-07-31 11:45:06 -0600

    adding main files: .sql .js . csv

--- schema.sql
+++ schema.sql
@@ -6,11 +6,11 @@ USE bamazon;
 CREATE table products (
   item_id INT NOT NULL,
   product_name VARCHAR(100) NULL,
-  department_name VARCHAR(100) NULL,
+  depart_name VARCHAR(100) NULL,
   price DECIMAL(10,4) NULL,
   stock_quantity INT NULL,
   PRIMARY KEY (item_id)
 );
 
-SELECT * FROM products;
+SELECT * FROM products ;
 
